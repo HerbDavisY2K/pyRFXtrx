46b21a1557915b8041c5b81412a9bc9f06cc8bb3
Daniel Høyer Iversen <mail@dahoiv.net>
2017-01-04 10:05:21 +0100

    Update __init__.py

--- RFXtrx/__init__.py
+++ RFXtrx/__init__.py
@@ -310,7 +310,7 @@ class SensorEvent(RFXtrxEvent):
                 or isinstance(pkt, lowlevel.TempHumid) \
                 or isinstance(pkt, lowlevel.TempHumidBaro):
             self.values['Temperature'] = pkt.temp
-        if isinstance(pkt, lowlevel.Bbq)
+        if isinstance(pkt, lowlevel.Bbq):
             self.values['Temperature'] = pkt.temp1
             self.values['Temperature2'] = pkt.temp2
         if isinstance(pkt, lowlevel.Humid) \
