c5758ccbce64c184331d29e47ae466eeab4cbd0c
Daniel Høyer Iversen <mail@dahoiv.net>
2017-01-04 09:58:47 +0100

    Update test_bbq.py

--- tests/test_bbq.py
+++ tests/test_bbq.py
@@ -9,7 +9,7 @@ class TempHumidityTestCase(TestCase):
         self.parser = RFXtrx.lowlevel.Bbq()
 
     def test_parse_bytes(self):
-        self.data = bytearray([0x0a 0x4e 0x01 0x06 0xfc 0xd8 0x00 0x13 0x00 0x13 0x79])
+        self.data = bytearray([0x0a, 0x4e, 0x01, 0x06, 0xfc, 0xd8, 0x00, 0x13, 0x00, 0x13, 0x79])
         bbq = RFXtrx.lowlevel.parse(self.data)
         self.assertEquals(RFXtrx.lowlevel.Bbq, type(bbq))
         self.assertEquals(temphum.temp1,16.7)
