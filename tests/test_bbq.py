3107ae1ca408191ac7ef5dcbfb3c35efddc391a4
Daniel Høyer Iversen <mail@dahoiv.net>
2017-01-04 10:14:11 +0100

    Update test_bbq.py

--- tests/test_bbq.py
+++ tests/test_bbq.py
@@ -12,8 +12,8 @@ class BbqTestCase(TestCase):
         self.data = bytearray([0x0a, 0x4e, 0x01, 0x06, 0xfc, 0xd8, 0x00, 0x13, 0x00, 0x13, 0x79])
         bbq = RFXtrx.lowlevel.parse(self.data)
         self.assertEquals(RFXtrx.lowlevel.Bbq, type(bbq))
-        self.assertEquals(bbq.temp1,16.7)
-        self.assertEquals(bbq.temp2,45)
-        self.assertEquals(bbq.type_string,'THGR810, THGN800')
-        self.assertEquals(bbq.id_string,'70:02')                
+        self.assertEquals(bbq.temp1,19)
+        self.assertEquals(bbq.temp2,19)
+        self.assertEquals(bbq.type_string,'BBQ1 - Maverick ET-732')
+        self.assertEquals(bbq.id_string,'fcd800:78')                
 
